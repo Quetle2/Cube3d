1287b665ecc9247cbcb0ae8ce2037c8371020b9c
Miguel Arada <marada@c1r5s5.42lisboa.com>
2026-01-20 16:00:02 +0000

    colisoes a funcionar

--- Main_SRC/player.c
+++ Main_SRC/player.c
@@ -6,7 +6,7 @@
 /*   By: marada <marada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 18:26:18 by marada            #+#    #+#             */
-/*   Updated: 2026/01/19 18:01:59 by marada           ###   ########.fr       */
+/*   Updated: 2026/01/20 15:54:29 by marada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,9 +49,88 @@ int	key_release(int keycode, t_player *player)
 	return (0);
 }
 
-void	move_down(char **map, t_player *player)
+void	move_down(char **map, t_player *player, int speed)
 {
-	
+	int	x_wall;
+	int y_wall;
+
+	if (map[(int)((player->y + 10 + speed) / 64)]
+		[(int)(player->x / 64)] == '1')
+	{
+		x_wall = (int)(player->x / 64) * 64;
+		y_wall = (int)((player->y + 10 + speed) / 64) * 64;
+		player->y += y_wall - (int)(player->y + 10) - 1;
+	}
+	else if(map[(int)((player->y + 10 + speed) / 64)]
+		[(int)((player->x + 10) / 64)] == '1')
+	{
+		x_wall = (int)(player->x + 10 / 64) * 64;
+		y_wall = (int)((player->y + 10 + speed) / 64) * 64;
+		player->y += y_wall - (int)(player->y + 10) - 1;
+	}
+}
+
+void	move_up(char **map, t_player *player, int speed)
+{
+	int	x_wall;
+	int y_wall;
+	printf("A%f : %f B%i\n", player->y, player->x, (int)((player->y - speed) / 64) * 64);
+	if (map[(int)((player->y - speed) / 64)]
+		[(int)(player->x / 64)] == '1')
+	{
+		x_wall = (int)(player->x / 64) * 64;
+		y_wall = (int)((player->y - speed) / 64) * 64;
+		player->y -= (int)(player->y) - y_wall - BLOCK - 1;
+	}
+	else if(map[(int)((player->y - speed) / 64)]
+		[(int)(player->x + 10 / 64)] == '1')
+	{
+		x_wall = (int)(player->x + 10 / 64) * 64;
+		y_wall = (int)((player->y - speed) / 64) * 64;
+		player->y -= (int)(player->y) - y_wall - BLOCK - 1;
+	}
+}
+
+void	move_left(char **map, t_player *player, int speed)
+{
+	int	x_wall;
+	int y_wall;
+
+	if (map[(int)((player->y) / 64)]
+		[(int)(player->x - speed / 64)] == '1')
+	{
+		x_wall = (int)(player->x - speed / 64) * 64;
+		y_wall = (int)((player->y) / 64) * 64;
+		player->x -= (int)(player->x) - x_wall - BLOCK - 1;
+	}
+	else if(map[(int)((player->y + 10) / 64)]
+		[(int)((player->x - speed) / 64)] == '1')
+	{
+		x_wall = (int)((player->x - speed) / 64) * 64;
+		y_wall = (int)((player->y + 10) / 64) * 64;
+		player->x -= (int)(player->x) - x_wall - BLOCK - 1;
+	}
+}
+
+void	move_right(char **map, t_player *player, int speed)
+{
+	int	x_wall;
+	int y_wall;
+
+	if (map[(int)((player->y) / 64)]
+		[(int)(player->x + 10 + speed/ 64)] == '1')
+	{
+		x_wall = (int)(player->x + 10 + speed / 64) * 64;
+		y_wall = (int)((player->y) / 64) * 64;
+		player->x += x_wall - (int)(player->x + 10) - 1;
+	}
+	else if(map[(int)((player->y + 10 + speed) / 64)]
+		[(int)((player->x + 10) / 64)] == '1')
+	{
+		x_wall = (int)(player->x + 10 + speed / 64) * 64;
+		y_wall = (int)((player->y + 10) / 64) * 64;
+		player->x += x_wall - (int)(player->x + 10) - 1;
+	}
 }
 
 void	move_player(t_game *game, t_player *player)
@@ -59,31 +138,36 @@ void	move_player(t_game *game, t_player *player)
 	int	speed;
 
 	speed = 5;
-	printf("sitiox :%i sitioy:%i\n", (int)player->x, (int)player->y);
 	if (player->key_left == 1 
-		&& game->map[(int)(player->y / 64)] 
-		[(int)((player->x - 2) / 64)] != '1'
-		&& game->map[(int)((player->y + 10) / 64)] 
-		[(int)((player->x - 2) / 64)] != '1')
+		&& (game->map[(int)(player->y / 64)] 
+		[(int)((player->x - speed) / 64)] == '1'
+		|| game->map[(int)((player->y + 10) / 64)] 
+		[(int)((player->x - speed) / 64)] == '1'))
+		move_left(game->map, player, speed);
+	else if (player->key_left == 1)
 		player->x -= speed;
 	if (player->key_up == 1
-		&& game->map[(int)((player->y - 2) / 64)]
-		[(int)(player->x / 64)] != '1'
-		&& game->map[(int)((player->y - 2) / 64)]
-		[(int)((player->x + 10) / 64)] != '1')
+		&& (game->map[(int)((player->y - speed) / 64)]
+		[(int)(player->x / 64)] == '1'
+		|| game->map[(int)((player->y - speed) / 64)]
+		[(int)((player->x + 10) / 64)] == '1'))
+		move_up(game->map, player, speed);
+	else if (player->key_up == 1)
 		player->y -= speed;
 	if (player->key_down == 1
-		&& game->map[(int)((player->y + 11) / 64)]
-		[(int)(player->x / 64)] != '1'
-		&& game->map[(int)((player->y + 11) / 64)]
-		[(int)((player->x + 10) / 64)] == '1')
-		move_down(game->map, player);
-	else
+		&& (game->map[(int)((player->y + 10 + speed) / 64)]
+		[(int)(player->x / 64)] == '1'
+		|| game->map[(int)((player->y + 10 + speed) / 64)]
+		[(int)((player->x + 10) / 64)] == '1'))
+		move_down(game->map, player, speed);
+	else if(player->key_down == 1)
 		player->y += speed;
 	if (player->key_right == 1
-		&& game->map[(int)(player->y / 64)]
-		[(int)((player->x + 11) / 64)] != '1'
-		&& game->map[(int)((player->y + 10) / 64.00)]
-		[(int)((player->x + 11) / 64)] != '1')
+		&& (game->map[(int)(player->y / 64)]
+		[(int)((player->x + 11) / 64)] == '1'
+		|| game->map[(int)((player->y + 10) / 64.00)]
+		[(int)((player->x + 11) / 64)] == '1'))
+		move_right(game->map, player, speed);
+	else if(player->key_right == 1)
 		player->x += speed;
 }
