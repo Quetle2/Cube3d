5161366a430f9a530c3749112f31e84be0275d6b
Miguel Arada <marada@c1r5s1.42lisboa.com>
2026-01-22 19:26:14 +0000

    esqueci de limpar

--- Main_SRC/interagir.c
+++ Main_SRC/interagir.c
@@ -6,7 +6,7 @@
 /*   By: marada <marada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 19:40:09 by marada            #+#    #+#             */
-/*   Updated: 2026/01/22 19:13:29 by marada           ###   ########.fr       */
+/*   Updated: 2026/01/22 19:25:55 by marada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ void	interagir(t_game *game, t_player *player)
 	if (ft_strchr("A", game->map[(int)((player->y - (player->sin_angle * SPEED)) / 64)] 
 		[(int)((player->x - (player->cos_angle * SPEED)) / 64)]) != NULL)
 	{
-		printf("ola1\n");
 		game->map[(int)((player->y - (player->sin_angle * SPEED)) / 64)] 
 		[(int)((player->x - (player->cos_angle * SPEED)) / 64)] = 'F';
-		printf("ola2\n");
 	}
 }
