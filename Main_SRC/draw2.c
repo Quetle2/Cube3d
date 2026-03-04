457d3ef3f0ea4381de1965f621d20077ee9ae08a
Miguel Arada <marada@c1r3s5.42lisboa.com>
2026-03-04 22:20:11 +0000

    norm

--- Main_SRC/draw2.c
+++ Main_SRC/draw2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jobraga- <jobraga-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marada <marada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 14:22:54 by marada            #+#    #+#             */
-/*   Updated: 2026/03/04 19:52:28 by jobraga-         ###   ########.fr       */
+/*   Updated: 2026/03/04 22:17:52 by marada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,34 @@
 
 int	touch_bola(float px, float py, t_list *bolas)
 {
-	t_bola *bola;
+	t_bola	*bola;
+
 	while (bolas)
 	{
-		bola = (t_bola*)(bolas->content);
+		bola = (t_bola *)(bolas->content);
 		if (bola == NULL)
 		{
 			bolas = bolas->next;
-			continue;
+			continue ;
 		}
-		if ((px >= bola->x && px <= (bola->x + FIRE)) && (py >= bola->y && py <= (bola->y + FIRE)))
-			return 1;
+		if ((px >= bola->x && px <= (bola->x + FIRE))
+			&& (py >= bola->y && py <= (bola->y + FIRE)))
+			return (1);
 		bolas = bolas->next;
 	}
-	return 0;
+	return (0);
 }
 
 int	touch(float px, float py, t_game *game)
 {
-	int	x = px / BLOCK;
-	int y = py / BLOCK;
+	int	x;
+	int	y;
+
+	px / BLOCK;
+	py / BLOCK;
 	if (ft_strchr("1A", game->map[y][x]))
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
 
 float	distance(float x, float y)
@@ -44,36 +49,42 @@ float	distance(float x, float y)
 	return (sqrt(x * x + y * y));
 }
 
-int get_side(float cos_angle, float sin_angle, float ray_x, float ray_y)
+void	get_side_helper(float cos_angle,
+		float sin_angle, float ray_x, float ray_y)
 {
 	float	dist_y;
 	float	dist_x;
-	// int	block_y;
-	// int	block_x;
 
-
-	ray_x += cos_angle;
-	ray_y += sin_angle;
 	if (cos_angle < 0)
 	{
 		ray_x -= 1;
-		dist_x = ((int)((ray_x + 64) / BLOCK) * 64) - ray_x;
+		dist_x = ((int)((ray_x + 64) / BLOCK)*64) - ray_x;
 	}
 	else
 	{
 		ray_x += 1;
-		dist_x = ((int)((ray_x - 64) / BLOCK) * 64 + 64) - ray_x;
+		dist_x = ((int)((ray_x - 64) / BLOCK)*64 + 64) - ray_x;
 	}
 	if (sin_angle < 0)
 	{
 		ray_y -= 1;
-		dist_y = ((int)((ray_y + 64) / BLOCK) * 64) - ray_y;
+		dist_y = ((int)((ray_y + 64) / BLOCK)*64) - ray_y;
 	}
 	else
 	{
 		ray_y += 1;
-		dist_y = ((int)((ray_y - 64) / BLOCK) * 64 + 64) - ray_y;
+		dist_y = ((int)((ray_y - 64) / BLOCK)*64 + 64) - ray_y;
 	}
+}
+
+int	get_side(float cos_angle, float sin_angle, float ray_x, float ray_y)
+{
+	float	dist_y;
+	float	dist_x;
+
+	ray_x += cos_angle;
+	ray_y += sin_angle;
+	get_side_helper(cos_angle, sin_angle, ray_x, ray_y);
 	if (fabs(dist_y) > fabs(dist_x))
 	{
 		if (dist_x < 0)
@@ -91,11 +102,11 @@ int get_side(float cos_angle, float sin_angle, float ray_x, float ray_y)
 	return (5);
 }
 
-void	draw_line(t_player *player, t_game *game, float start_x, int i)
+typedef struct s_vars
 {
 	float	cos_angle;
 	float	sin_angle;
-	float	dist = 0;
+	float	dist;
 	float	height;
 	int		start_y;
 	int		end;
@@ -103,145 +114,154 @@ void	draw_line(t_player *player, t_game *game, float start_x, int i)
 	int		side;
 	float	proj_plane;
 	int		wall_height;
-	int		stepX;
-	int		stepY;
-	float	sideDistX;
-	float	sideDistY;
-	float	deltaDistX = 0;
-	float	deltaDistY = 0;
-	int		mapX;
-	int		mapY;
+	int		stepx;
+	int		stepy;
+	float	sidedistx;
+	float	sidedisty;
+	float	deltadistx;
+	float	deltadisty;
+	int		mapx;
+	int		mapy;
+	int		start_y_original;
+	double	wall_x;
+	int		tex_x;
+	int		tex_y;
+	int		color;
+	float	ray_x;
+	float	ray_y;
+}	t_vars;
 
-	cos_angle = cos(start_x);
-	sin_angle = sin(start_x);
+void	draw_line(t_player *player, t_game *game, float start_x, int i)
+{
+	t_vars	vars;
 
-	mapX = (int)(player->x / BLOCK);
-	mapY = (int)(player->y / BLOCK);
-
-	deltaDistX = fabs(BLOCK / cos_angle);
-	deltaDistY = fabs(BLOCK / sin_angle);
-
-	if (cos_angle < 0)
+	vars.ray_x = player->x;
+	vars.ray_y = player->y;
+	vars.deltadisty = 0;
+	vars.deltadistx = 0;
+	vars.dist = 0;
+	vars.cos_angle = cos(start_x);
+	vars.sin_angle = sin(start_x);
+	vars.mapx = (int)(player->x / BLOCK);
+	vars.mapy = (int)(player->y / BLOCK);
+	vars.deltadistx = fabs(BLOCK / vars.cos_angle);
+	vars.deltadisty = fabs(BLOCK / vars.sin_angle);
+	if (vars.cos_angle < 0)
 	{
-		stepX = -1;
-		sideDistX = (player->x - mapX * BLOCK) / fabs(cos_angle);
+		vars.stepx = -1;
+		vars.sidedistx = (player->x - vars.mapx * BLOCK) / fabs(vars.cos_angle);
 	}
 	else
 	{
-		stepX = 1;
-		sideDistX = ((mapX + 1) * BLOCK - player->x) / fabs(cos_angle);
+		vars.stepx = 1;
+		vars.sidedistx = ((vars.mapx + 1)
+				* BLOCK - player->x) / fabs(vars.cos_angle);
 	}
-	if (sin_angle < 0)
+	if (vars.sin_angle < 0)
 	{
-		stepY = -1;
-		sideDistY = (player->y - mapY * BLOCK) / fabs(sin_angle);
+		vars.stepy = -1;
+		vars.sidedisty = (player->y - vars.mapy * BLOCK) / fabs(vars.sin_angle);
 	}
 	else
 	{
-		stepY = 1;
-		sideDistY = ((mapY + 1) * BLOCK - player->y) / fabs(sin_angle);
+		vars.stepy = 1;
+		vars.sidedisty = ((vars.mapy + 1)
+				* BLOCK - player->y) / fabs(vars.sin_angle);
 	}
-	hit = 0;
-	while (!hit)
+	vars.hit = 0;
+	while (!vars.hit)
 	{
-		if (sideDistX < sideDistY)
+		if (vars.sidedistx < vars.sidedisty)
 		{
-			sideDistX += deltaDistX;
-			mapX += stepX;
-			if (stepX == 1)
-				side = 1;
+			vars.sidedistx += vars.deltadistx;
+			vars.mapx += vars.stepx;
+			if (vars.stepx == 1)
+				vars.side = 1;
 			else
-				side = 2;
+				vars.side = 2;
 		}
 		else
 		{
-			sideDistY += deltaDistY;
-			mapY += stepY;
-			if (stepY == 1)
-				side = 3;
+			vars.sidedisty += vars.deltadisty;
+			vars.mapy += vars.stepy;
+			if (vars.stepy == 1)
+				vars.side = 3;
 			else
-				side = 4;
+				vars.side = 4;
 		}
-		if (ft_strchr("1A", game->map[mapY][mapX]))
-			hit = 1;
+		if (ft_strchr("1A", game->map[vars.mapy][vars.mapx]))
+			vars.hit = 1;
 	}
-	if (side == 1 || side == 2)
-		dist = (mapX * BLOCK - player->x + (1 - stepX) * BLOCK / 2) / cos_angle;
-	else if (side == 3 || side == 4)
-		dist = (mapY * BLOCK - player->y + (1 - stepY) * BLOCK / 2) / sin_angle;
-	proj_plane = WIDTH / (2 * tan(PI / 6));
-	wall_height = (int)((BLOCK / dist) * proj_plane);
-	if (hit == 1)
-		height = (BLOCK / dist) * (WIDTH / 2);
+	if (vars.side == 1 || vars.side == 2)
+		vars.dist = (vars.mapx * BLOCK - player->x
+				+ (1 - vars.stepx) * BLOCK / 2) / vars.cos_angle;
+	else if (vars.side == 3 || vars.side == 4)
+		vars.dist = (vars.mapy * BLOCK - player->y
+				+ (1 - vars.stepy) * BLOCK / 2) / vars.sin_angle;
+	vars.proj_plane = WIDTH / (2 * tan(PI / 6));
+	vars.wall_height = (int)((BLOCK / vars.dist) * vars.proj_plane);
+	if (vars.hit == 1)
+		vars.height = (BLOCK / vars.dist) * (WIDTH / 2);
 	else
-		height = (FIRE / dist) * (WIDTH / 2);
-	(void)height;
-	start_y = -wall_height / 2 + HEIGHT / 2;
-	int		start_y_original;
-	start_y_original = start_y;
-	if (start_y < 0)
-		start_y = 0;
-	end = wall_height / 2 + HEIGHT / 2;
-	if (end >= HEIGHT)
-		end = HEIGHT - 1;
-
-	//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-	double	wall_x;
-	int		tex_x;
-	if (side == 1 || side == 2)
-    	wall_x = fmod(player->y + dist * sin_angle, BLOCK);
+		vars.height = (FIRE / vars.dist) * (WIDTH / 2);
+	(void)vars.height;
+	vars.start_y = -vars.wall_height / 2 + HEIGHT / 2;
+	vars.start_y_original = vars.start_y;
+	if (vars.start_y < 0)
+		vars.start_y = 0;
+	vars.end = vars.wall_height / 2 + HEIGHT / 2;
+	if (vars.end >= HEIGHT)
+		vars.end = HEIGHT - 1;
+	if (vars.side == 1 || vars.side == 2)
+		vars.wall_x = fmod(player->y + vars.dist * vars.sin_angle, BLOCK);
 	else
-    	wall_x = fmod(player->x + dist * cos_angle, BLOCK);
-	tex_x = (int)(wall_x / BLOCK * 128);
-	int		tex_y;
-	int		color;
-	while (start_y < end)
+		vars.wall_x = fmod(player->x + vars.dist * vars.cos_angle, BLOCK);
+	vars.tex_x = (int)(vars.wall_x / BLOCK * 128);
+	while (vars.start_y < vars.end)
 	{
-    	tex_y = (start_y - start_y_original) * 128 / wall_height;
-    	if (side == 1)
-        	color = game->textures[EAST][tex_y * 128 + tex_x];
-    	else if (side == 2)
-        	color = game->textures[WEST][tex_y * 128 + tex_x];
-    	else if (side == 3)
-        	color = game->textures[SOUTH][tex_y * 128 + tex_x];
-    	else
-        	color = game->textures[NORTH][tex_y * 128 + tex_x];
-    	put_pixel(i, start_y, color, game);
-    	start_y++;
+		vars.tex_y = (vars.start_y - vars.start_y_original)
+			* 128 / vars.wall_height;
+		if (vars.side == 1)
+			vars.color = game->textures[EAST][vars.tex_y * 128 + vars.tex_x];
+		else if (vars.side == 2)
+			vars.color = game->textures[WEST][vars.tex_y * 128 + vars.tex_x];
+		else if (vars.side == 3)
+			vars.color = game->textures[SOUTH][vars.tex_y * 128 + vars.tex_x];
+		else
+			vars.color = game->textures[NORTH][vars.tex_y * 128 + vars.tex_x];
+		put_pixel(i, vars.start_y, vars.color, game);
+		vars.start_y++;
 	}
-//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa
-	float	ray_x = player->x;
-	float	ray_y = player->y;
-	hit = 0;
-	while (!touch_bola(ray_x, ray_y, game->bola) && (ray_x <= game->mapinfo.height * 64 && ray_x >= 0) && (ray_y <= game->mapinfo.width * 64 && ray_y >= 0))
+	vars.hit = 0;
+	while (!touch_bola(vars.ray_x, vars.ray_y, game->bola)
+		&& (vars.ray_x <= game->mapinfo.height * 64 && vars.ray_x >= 0)
+		&& (vars.ray_y <= game->mapinfo.width * 64 && vars.ray_y >= 0))
 	{
-		// printf("%i,%i\n", game->mapinfo.height * 64, game->mapinfo.width * 64);
-		ray_x += cos_angle;
-		ray_y += sin_angle;
+		vars.ray_x += vars.cos_angle;
+		vars.ray_y += vars.sin_angle;
 	}
-	if (touch_bola(ray_x, ray_y, game->bola))
-		hit = 1;
-
-	dist = distance(ray_x - player->x, ray_y - player->y);
-	height = (FIRE / dist) * (WIDTH / 2);
-	start_y = (HEIGHT - height) / 2;
-	end = start_y + height;
-	if (hit == 1)
+	if (touch_bola(vars.ray_x, vars.ray_y, game->bola))
+		vars.hit = 1;
+	vars.dist = distance(vars.ray_x - player->x, vars.ray_y - player->y);
+	vars.height = (FIRE / vars.dist) * (WIDTH / 2);
+	vars.start_y = (HEIGHT - vars.height) / 2;
+	vars.end = vars.start_y + vars.height;
+	if (vars.hit == 1)
 	{
-		while (start_y < end)
+		while (vars.start_y < vars.end)
 		{
-			put_pixel(i, start_y, 100, game);
-			start_y++;
+			put_pixel(i, vars.start_y, 100, game);
+			vars.start_y++;
 		}
 	}
 }
 
 void	draw_background(t_game *game)
 {
-	int		x;
-	int		y;
-	int		*floor;
-	int		*ceiling;
+	int	x;
+	int	y;
+	int	*floor;
+	int	*ceiling;
 
 	y = 0;
 	floor = game->texinfo.floor;
@@ -251,8 +271,10 @@ void	draw_background(t_game *game)
 		x = 0;
 		while (x < WIDTH)
 		{
-			put_pixel(x, y, (ceiling[0] << 16) | (ceiling[1] << 8) | ceiling[2], game);
-			put_pixel(x, (y + HEIGHT / 2), (floor[0] << 16) | (floor[1] << 8) | floor[2], game);
+			put_pixel(x, y, (ceiling[0] << 16)
+				| (ceiling[1] << 8) | ceiling[2], game);
+			put_pixel(x, (y + HEIGHT / 2), (floor[0] << 16)
+				| (floor[1] << 8) | floor[2], game);
 			x++;
 		}
 		y++;
@@ -262,19 +284,20 @@ void	draw_background(t_game *game)
 int	draw_loop(t_game *game)
 {
 	t_player	*player;
+	float		fraction;
+	float		start_x;
+	int			i;
 
+	i = 0;
+	start_x = player->angle - PI / 6;
+	fraction = PI / 3 / WIDTH;
 	player = &game->player;
 	move_player(game, player);
 	move_bolas(game);
 	clear_image(game);
 	draw_background(game);
-	// draw_square(player->x /*/ 2*/, player->y /*/ 2*/, 10, game);
-	// draw_map(game);
-	// draw_bolas(game, game->bola);
-
-	float	fraction = PI / 3 / WIDTH;
-	float	start_x = player->angle - PI / 6;
-	int		i = 0;
+	draw_square(player->x / 2, player->y / 2, 10, game);
+	draw_map(game);
 	while (i < WIDTH)
 	{
 		draw_line(player, game, start_x, i);
